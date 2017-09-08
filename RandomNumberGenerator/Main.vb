633b5a78ee34142ddd9968dccca87dedc941e4fe
jtrent238 <jtrent238@outlook.com>
2017-09-08 17:34:14 -0400

    1.0.0.0

---
+++ RandomNumberGenerator/Main.vb
@@ -0,0 +1,45 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Net
+Imports SharpRaven
+Imports SharpRaven.Data
+
+Public Class Main
+
+    Dim ravenClient = New RavenClient("https://775175151d2d4c269c3da506af04292e:0985988884744275b99b32be5c61be03@sentry.io/214659")
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+        Catch exception As Exception
+            ravenClient.Capture(New SentryEvent(exception))
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim randomValue = CInt(Math.Floor((TextBox2.Text.ToString - TextBox3.Text.ToString + 1) * Rnd())) + TextBox3.Text.ToString
+            TextBox1.Text = randomValue
+        Catch exception As Exception
+            ravenClient.Capture(New SentryEvent(exception))
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            YUCLICKME.Show()
+        Catch exception As Exception
+            ravenClient.Capture(New SentryEvent(exception))
+        End Try
+    End Sub
+End Class
